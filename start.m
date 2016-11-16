964a4802fd0313ae11e8ad37577502290963e940
Pouneh A <Pouneh A>
2016-11-15 16:51:57 -0800

    fix data import function

--- start.m
+++ start.m
@@ -1,42 +1,47 @@
-function [numdata, strdata] = readDat()
+function [data, categoriacl] = readData()
 
-fileName = "ecs171.dataset.small.txt";
-fid = fopen(fileName,'rt');
-delim = " ";
+	fileName = "ecs171.dataset.small.txt";
+	fid = fopen(fileName,'rt');
 
-line = fgets(fid);
+	line = fgetl(fid);
+	titles = strsplit(line);
 
-if (line == -1)
-	% abort program
-else
-if (line ~= -1)	
-	while (TRUE)
-		line = fgets(fid)
-		if (line == -1)
-			break;
-		endif
+	data = []; 
+	categorical = [];
 
-		elems = strsplit(line, delim);
-		nums = str2double(elems);
-		nans = isnan(nums);
+	count = 0
 
-		idxnans = find(nans);
+	if (line == -1)
+		% abort program
+	else
+		while (line ~= -1)
+			line = fgetl(fid);
+			if (line == -1)
+				break;
+			endif
 
-		if any(ismember(idxnans, i))
-			varargout{i}{end+1} = elems{i};
-        else
-        	varargout{i}{end+1} = nums(i);
-        endif
-	endwhile
-endif
+			elems = strsplit(line);
+			nums = str2double(elems);
+			if( any(isnan(nums(4502))))
+				nums(4502) = -1;
+			endif
+
+			numsModified = nums(!isnan(nums));
+			strdat = {elems{isnan(nums)}} ; 
+
+			data = [data;numsModified];
+			if(sum(size(categorical)) == 0)
+				categorical = strdat;
+			else
+				categorical = [categorical; strdat];
+			endif
+
+			count = count + 1
+		endwhile
+	endif
+	fclose(fid);
+
+endfunction
 
 
 
-count = 0;
-while(line ~= -1)
-		line = fgets(fid)
-		if (line == -1)
-			break;
-		endif
-		count = count + 1;
-endwhile
